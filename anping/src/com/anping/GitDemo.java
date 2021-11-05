af56ef0e442049714598571e8628f9f1c36eca17
feng <1162636418@qq.com>
2021-11-05 15:28:03 +0800

    安平道按察使林

---
+++ anping/src/com/anping/GitDemo.java
@@ -0,0 +1,13 @@
+package com.anping;
+
+/**
+ * @Auther: lovvyang学java
+ * @Date:2021/11/5 - 11 - 05 - 15:22
+ * @Description: com.anping
+ * @version: 1.0
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("大唐 安平道按察使 平成郡公 左御史大夫 英武殿大学士 林");
+    }
+}
